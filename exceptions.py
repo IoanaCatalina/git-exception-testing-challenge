70ddf0901c900d4aa6f35bc6e62bc6be94b3500c
ioanamosteanu <ioana.mosteanu@gmail.com>
2022-03-24 21:49:06 +0000

    final version Ayan

--- exceptions.py
+++ exceptions.py
@@ -1,61 +1,60 @@
-BROAD REASONS WHY YOU MIGHT GET AN EXCEPTION
-(1) Trying to refer to something that doesn't exist
-(2) Using a value that is inappropriate in some way
+# BROAD REASONS WHY YOU MIGHT GET AN EXCEPTION
+# (1) Trying to refer to something that doesn't exist
+# (2) Using a value that is inappropriate in some way
+#
+# CORE EXAMPLES OF EXCEPTIONS IN THIS FILE
+# AttributeError (1)
+# KeyError (1)
+# IndexError (1)
+# NameError (1)
+# UnboundLocalError (1)
+# TypeError (2)
+# ValueError (2)
+# ZeroDivisionError (2)
+# OverflowError (2)
+# FileNotFoundError (1)
+# UnicodeEncodeError (2)
+# ModuleNotFoundError (1)
+# ImportError (1)
+#
+# BONUS EXAMPLES YOU CAN TRY IF YOU WISH
+# PermissionError (2)
+# IsADirectoryError (2)
 
-CORE EXAMPLES OF EXCEPTIONS IN THIS FILE
-AttributeError (1)
-KeyError (1)
-IndexError (1)
-NameError (1)
-UnboundLocalError (1)
-TypeError (2)
-ValueError (2)
-ZeroDivisionError (2)
-OverflowError (2)
-FileNotFoundError (1)
-UnicodeEncodeError (2)
-ModuleNotFoundError (1)
-ImportError (1)
 
-BONUS EXAMPLES YOU CAN TRY IF YOU WISH
-PermissionError (2)
-IsADirectoryError (2)
-
-
-AttributeError - EXAMPLE
+# AttributeError - EXAMPLE
 def produce_attribute_error():
     print(1.234.upper())
 
-produce_attribute_error()
 
-KeyError
+# KeyError
 food_prices = {
     'milk':  1.2,
     'apples': 0.5,
     'oranges': 0.34,
     'cucumber': 0.7
     }
-def key_error():
+def produce_key_error():
         print(food_prices['bread'])
-key_error()
+# key_error()
 
 
-IndexError
+# IndexError
 drinks = ['beer', 'wine', 'water', 'juice', 'coffee']
 def produce_index_error():
     print(drinks[8])
-produce_index_error()
+# produce_index_error()
 
-NameError
+# NameError
 def produce_name_error():
     print(b)
-produce_name_error()
+# produce_name_error()
 
-UnboundLocalError
+# UnboundLocalError
 def produce_unbound_local_error():
     print(a)
     a = 'How are you?'
-produce_unbound_local_error()
+# produce_unbound_local_error()
 
 
 # TypeError
@@ -63,48 +62,48 @@ def produce_type_error():
     name = 'Ayan'
     age = 23
     print(name+age)
-produce_type_error()
+# produce_type_error()
 
-ValueError
+# ValueError
 import math
 def produce_value_error():
     b = math.sqrt(-5)
     print(b)
-produce_value_error()
+# produce_value_error()
 
-ZeroDivisionError
+# ZeroDivisionError
 def produce_zero_division_error():
     a = 5 //0
     print(a)
-produce_zero_division_error()
+# produce_zero_division_error()
 
-OverflowError
+# OverflowError
 
 def produce_overflow_error():
-    a = 10000000000000000
+    a = 10.0
     b = a**1000000000000
     print(b)
-produce_overflow_error()
+# produce_overflow_error()
 
-FileNotFoundError
+# FileNotFoundError
 def produce_file_not_found_error():
-    with open('todo.txt', 'r') as todofile:
-        read = todofile1.read()
+    with open('todo5.txt', 'r') as todofile:
+        read = todofile.read()
         print(read)
-produce_file_not_found_error()
-UnicodeEncodeError
+# produce_file_not_found_error()
+# UnicodeEncodeError
 
 def produce_unicode_encode_error():
     print(type(NULL))
-produce_unicode_encode_error()
+# produce_unicode_encode_error()
 
-ModuleNotFoundError()
+# ModuleNotFoundError()
 
 def produce_module_not_found_error():
    import b
-produce_module_not_found_error()
+# produce_module_not_found_error()
 
 # ImportError
 def produce_import_error():
     from math import time
-produce_import_error()
+# produce_import_error()
